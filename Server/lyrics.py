4d7acf2204e593d836a058fcbed5d01e957040f1
elor666 <89981508+elor666@users.noreply.github.com>
2023-03-11 12:24:08 +0200

    download and lyrics extarction upgrade
    downloads the song to buffer instead to disk. added feature to check if the lyrics exist, and not download it.

--- Server/lyrics.py
+++ Server/lyrics.py
@@ -1,10 +1,11 @@
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
 from pydub import AudioSegment
-import os
 from bs4 import BeautifulSoup
 import requests
 from pathlib import Path
+from io import BytesIO
+import os
 
 
 Num_Search = 5
@@ -12,30 +13,54 @@ Max_duration = 420 #7 minutes long
 
 
 def extract_lyrics(artist_name, song_name):
+    """downloads lyrics if have them on the site 'azlyrics.com' and if not downloaded yet."""
+
     if not Path(f"SongsDetails\\{artist_name} {song_name}\\lyrics.txt").is_file():
         link = 'https://www.azlyrics.com/lyrics/'+artist_name.replace(' ', '').lower()+'/'+song_name.replace(' ', '').lower()+".html"
 
         req = requests.get(link)
 
-        soup = BeautifulSoup(req.content.decode(),"html.parser")#make soup that is parse-able by bs
-        so = set(soup.get_text().split("\n\n\n"))
-        so = list(so)
+        soup = BeautifulSoup(req.content.decode(),"html.parser")
 
-        so.remove('')
-        lyrics = max(so, key=len)
-        if lyrics == "Welcome to AZLyrics!\r\n              It's a place where all searches end!\r\n              We have a large, legal, every day growing universe of lyrics where stars of all genres and ages shine.":
-            print("Lyrics not found")
-            return False
-        #print(lyrics[2:])
-        if not Path(f"SongsDetails//{artist_name.lower()} {song_name.lower()}").exists():
-            os.makedirs(f"SongsDetails//{artist_name.lower()} {song_name.lower()}")
-            
-        with open(f"SongsDetails//{artist_name.lower()} {song_name.lower()}//lyrics.txt","w") as file:
-            file.write(lyrics[2:])
+        temp_soup = soup.find("div",{"class":"col-xs-12 col-lg-8 text-center"})
+
+        if temp_soup != None:
+            lyrics = temp_soup.find("div",{"class":""}).get_text().strip()
+
+            if lyrics != None:
+                if not Path(f"SongsDetails//{artist_name.lower()} {song_name.lower()}").exists():
+                    os.makedirs(f"SongsDetails//{artist_name.lower()} {song_name.lower()}")
+                    
+                with open(f"SongsDetails//{artist_name.lower()} {song_name.lower()}//lyrics.txt","w") as file:
+                    file.write(lyrics)
         
-        print("Got Lyrics")
+                print("Got Lyrics")
+                return True
+        
+        return False
     return True
+
+def is_lyrics(artist_name, song_name):  
+    """return whether lyrics exists or not"""
+    if not Path(f"SongsDetails\\{artist_name} {song_name}\\lyrics.txt").is_file():
+        
+        link = 'https://www.azlyrics.com/lyrics/'+artist_name.replace(' ', '').lower()+'/'+song_name.replace(' ', '').lower()+".html"
+
+        req = requests.get(link)
+
+        soup = BeautifulSoup(req.content.decode(),"html.parser")
+
+        temp_soup = soup.find("div",{"class":"col-xs-12 col-lg-8 text-center"})
+
+        if temp_soup != None:
+            lyrics = temp_soup.find("div",{"class":""}).get_text().strip()
+
+            if lyrics != None:
+                return True
     
+        return False
+
+    return True
 
 def download_song_lyrics(artist_name, song_name, search_index):
 
@@ -53,27 +78,25 @@ def download_song_lyrics(artist_name, song_name, search_index):
             print(r["result"][search_index]["title"])
 
             print(url)
-
+            data = BytesIO()
             yt = YouTube(url)
-            strem = yt.streams.filter(only_audio=True)[0]
-            out_file = strem.download("./Songs")
-
-            base, ext = os.path.splitext(out_file)
-            new_file = r"./Songs/"+f"{artist_name} {song_name}"+".ogg"
-            AudioSegment.from_file(out_file).export(new_file, format="ogg")
-            os.remove(out_file)
+            strem = yt.streams.filter(only_audio=True).asc()[0]
+            strem.stream_to_buffer(data)
+            data.seek(0)
+            path_file = r"Songs/"+f"{artist_name} {song_name}"+".ogg"
+            AudioSegment.from_file(data).export(path_file,format="ogg")
         
-        return new_file
+        return path_file
     
     return False
 
 def str_time(timestr):
+    """returns the number of seconds from a time string format H:M:S"""
     ftr = [3600,60,1][-(len(timestr.split(":"))):3]
-    #print(sum([a*b for a,b in zip(ftr, map(int,timestr.split(':')))]))
     return sum([a*b for a,b in zip(ftr, map(int,timestr.split(':')))])
 
 def search_result(artist_name, song_name):
-    if extract_lyrics(artist_name, song_name):
+    if is_lyrics(artist_name, song_name):
         if not Path(f"Songs\\{artist_name} {song_name}.ogg").is_file():
             videosSearch = VideosSearch(f"{artist_name} {song_name}", limit = Num_Search)
             res = videosSearch.result()
@@ -82,9 +105,9 @@ def search_result(artist_name, song_name):
             return [f"{artist_name} {song_name}"]
     return []
 
+	
 
-#download_song_lyrics("stellar", "bad dream", 0)
-"""print(search_result("stellar", "bad dream"))
-index = int(input())
-print(download_song_lyrics("stellar", "bad dream",index))"""
+#get_song_text("stellar", "cold outside")
 
+#print(new_file)
+#print(strem)
